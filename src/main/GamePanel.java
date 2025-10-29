667ee972b257bcdab6cf84db998ecd48997c46df
6704062611280 <s6704062611280@email.kmutnb.ac.th>
2025-10-29 14:23:17 +0700

    Update sprite and Animation Slime

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,12 +7,14 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import entity.Player;
+
 public class GamePanel extends JPanel implements Runnable {
     // screen settting
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 40x40 tile
+    public final int tileSize = originalTileSize * scale; // 40x40 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
 
@@ -24,6 +26,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyH);
 
     // set player's defalut positions
     int playerX = 100;
@@ -76,16 +79,7 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-
-        } else if (keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if (keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if (keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -93,10 +87,8 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
-
+        
+        player.draw(g2);
         g2.dispose();
     }
 }
